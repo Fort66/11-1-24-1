a42c01963a6285035838ee1adf1821e25f934152
Oleg <mamonov.1966@yandex.ru>
2025-03-05 10:26:36 +0300

    update files

--- UI/Buttons/class_ButtonImage3.py
+++ UI/Buttons/class_ButtonImage3.py
@@ -9,21 +9,22 @@ from icecream import ic
 
 pg.init()
 
+
 @dataclass
 class ButtonImage3:
     pos: tuple = (0, 0)
     size: tuple = (0, 0)
 
     text: str = None
-    textColor: str | tuple[int, int, int] = 'white'
+    textColor: str | tuple[int, int, int] = "white"
     font: InitVar[str] = None
     font_size: int = 26
-    disabledColorText: str | tuple[int, int, int] = 'grey'
+    disabledColorText: str | tuple[int, int, int] = "grey"
 
     image: InitVar[str] = None
     shadingImage: InitVar[str] = None
     disabledImage: InitVar[str] = None
-    
+
     flipVertical: bool = False
     flipHorizontal: bool = False
     onEnabled: bool = True
@@ -31,18 +32,36 @@ class ButtonImage3:
     isClicked: bool = False
 
     onClickReference: object = None
-    referenceArgs: tuple = field(default_factory = tuple)
-    referenceKwargs: dict = field(default_factory = dict)
+    referenceArgs: tuple = field(default_factory=tuple)
+    referenceKwargs: dict = field(default_factory=dict)
 
-    def __post_init__(self, font: str, image: str, shadingImage: str, disabledImage: str):
-        self.image = flip(scale(load(image).convert_alpha(), self.size), self.flipHorizontal, self.flipVertical)
-        self.rect = self.image.get_rect(x = self.pos[0], y = self.pos[1])
+    def __post_init__(
+        self, font: str, image: str, shadingImage: str, disabledImage: str
+    ):
+        self.image = flip(
+            scale(load(image).convert_alpha(), self.size),
+            self.flipHorizontal,
+            self.flipVertical,
+        )
+        self.rect = self.image.get_rect(x=self.pos[0], y=self.pos[1])
 
-        self.shadingImage = flip(scale(load(shadingImage).convert_alpha(), self.size), self.flipHorizontal, self.flipVertical)
-        
-        self.disabledImage = flip(scale(load(disabledImage).convert_alpha(), self.size), self.flipHorizontal, self.flipVertical)
+        self.shadingImage = flip(
+            scale(load(shadingImage).convert_alpha(), self.size),
+            self.flipHorizontal,
+            self.flipVertical,
+        )
 
-        self.font = pg.font.Font(font, self.font_size) if font else pg.font.SysFont('Arial', self.font_size)
+        self.disabledImage = flip(
+            scale(load(disabledImage).convert_alpha(), self.size),
+            self.flipHorizontal,
+            self.flipVertical,
+        )
+
+        self.font = (
+            pg.font.Font(font, self.font_size)
+            if font
+            else pg.font.SysFont("Arial", self.font_size)
+        )
 
     def handleEvent(self, event):
         if self.onEnabled:
@@ -52,7 +71,9 @@ class ButtonImage3:
                 if event.button == 1 and self.isHovered:
                     self.isClicked = True
                     if self.onClickReference:
-                        self.onClickReference(*self.referenceArgs, **self.referenceKwargs)
+                        self.onClickReference(
+                            *self.referenceArgs, **self.referenceKwargs
+                        )
             if event.type == MOUSEBUTTONUP:
                 self.isClicked = False
 
@@ -66,13 +87,15 @@ class ButtonImage3:
                 surface.blit(self.shadingImage, self.rect)
         else:
             surface.blit(self.disabledImage, self.rect)
-            
+
         if self.text:
             if self.onEnabled:
                 textSurface = self.font.render(self.text, True, self.textColor)
             else:
                 textSurface = self.font.render(self.text, True, self.disabledColorText)
-                textSurface.fill(self.disabledColorText, special_flags = pg.BLEND_RGBA_MULT)
-            textRect = textSurface.get_rect(center = self.rect.center)
-            
+                textSurface.fill(
+                    self.disabledColorText, special_flags=pg.BLEND_RGBA_MULT
+                )
+            textRect = textSurface.get_rect(center=self.rect.center)
+
             surface.blit(textSurface, textRect)
